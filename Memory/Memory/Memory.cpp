2349423a550f64deab20fab222e39944f8449749
usm <usm9401@gmail.com>
2023-12-13 10:49:01 +0900

    2023-12-13 AK

---
+++ Memory/Memory/Memory.cpp
@@ -0,0 +1,22 @@
+﻿// Memory.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+void PlayerMove() {
+    int MonsterSpeed = 3;
+}
+void MonsterMove();
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    PlayerMove();
+    MonsterMove();
+
+    
+
+}
