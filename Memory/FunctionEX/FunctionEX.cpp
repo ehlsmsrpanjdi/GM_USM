2937237fe58e75184aa3e7ac6ffabe9934ff1e03
usm <usm9401@gmail.com>
2023-12-14 15:36:09 +0900

    과제2

---
+++ Memory/FunctionEX/FunctionEX.cpp
@@ -0,0 +1,20 @@
+﻿// FunctionEX.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+//함수의 리턴값 쪽에 자료형을 
+void Test() {
+
+}
+
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	b = std::rand();
+}
---
+++ Memory/Solution2/Solution2.cpp
@@ -0,0 +1,23 @@
+﻿// Solution2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Test(int _Value0, int _Value1, int _Value2, int _Value3) {
+	int* Addresses[4] = { &_Value0,&_Value1 ,&_Value2 ,&_Value3 };
+	__int64 Address0 = reinterpret_cast<__int64>(Addresses[0]);
+	__int64 Address1 = reinterpret_cast<__int64>(Addresses[1]);
+	__int64 Address2 = reinterpret_cast<__int64>(Addresses[2]);
+	__int64 Address3 = reinterpret_cast<__int64>(Addresses[3]);
+}
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	Test(a, b, c, d);
+
+}
+
