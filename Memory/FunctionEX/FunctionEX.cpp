337126d149e9f73d56ef457d678b97b785f2eac2
usm <usm9401@gmail.com>
2023-12-15 15:21:07 +0900

    과제와 2023-12-15 수업

--- Memory/FunctionEX/FunctionEX.cpp
+++ Memory/FunctionEX/FunctionEX.cpp
@@ -1,15 +1,4 @@
-﻿// FunctionEX.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
-
-//함수의 리턴값 쪽에 자료형을 
-void Test() {
-
-}
-
-
+﻿#include <iostream>
 
 int main()
 {
