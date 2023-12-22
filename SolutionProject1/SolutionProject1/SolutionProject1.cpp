2fb9704cb34b28dd8923655aedbc7fd490c36e7c
usm <usm9401@gmail.com>
2023-12-22 16:32:24 +0900

    프로그래머스1

--- SolutionProject1/SolutionProject1/SolutionProject1.cpp
+++ SolutionProject1/SolutionProject1/SolutionProject1.cpp
@@ -1,12 +1,42 @@
-﻿// SolutionProject1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <string>
+﻿#include <string>
 #include <vector>
 
 using namespace std;
 
-int main()
-{
-	vector<vector<bool>> Map;
+vector<vector<int>> solution(int n) {
+    int Count = 0;
+    int RCount = 1;
+    int Temp = 0;
+    int X = 0;
+    int Y = 0;
+    int BoolX = true;
+    int BoolY = false;
+    vector<vector<int>> v1;
+    vector<int> v2(n);
+    for (int i = 0; i < n; ++i) {
+        v1.push_back(v2);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        v1[0][i] = i;
+    }
+
+    while (Count != n * n) {
+
+        Temp = n;
+        while (Temp - Count) {
+            v1[0][n - ];
+            --Temp;
+        }
+
+
+
+        ++Count;
+        if (Count == 2) {
+            ++RCount;
+            Count = 0;
+        }
+    }
+    vector<vector<int>> answer;
+    return answer;
 }
