61472f48e3d408dc2a1893dfc97e97af4b64c35f
usm <usm9401@gmail.com>
2024-01-09 10:06:51 +0900

    .

--- Memory/Test/Test.cpp
+++ Memory/Test/Test.cpp
@@ -1,53 +1,125 @@
-﻿// Test.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// Array.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
-#include <testengine/TestConsoleScreen.h>
-#include <..\Memory\TestPhysics\Gravity.h>
-#include <Windows.h>
-#include <assert.h>
+#include <ConsoleEngine/EngineDebug.h>
 
-int function(int a, int b, int c) {
-	return a;
-}
 
-int function1(int a) {
-	return a;
-}
-
-int function2(int a) {
-	return a;
-}
-
-int function3(int a) {
-	return a;
-}
-
-class A {
+class IntArray
+{
+	// private: 디폴트 접근제한 지정자
 public:
-	void test() {
-		int a = 0;
+	// 디폴트 생성자
+	IntArray(int _Size)
+	{
+		ReSize(_Size);
 	}
-	void test2() {
 
-	}
-	void test3() {
+	// 디폴트 복사 생성자
+	IntArray(const IntArray& _Other)
+	{
+		ReSize(_Other.NumValue);
 
+		NumValue = _Other.NumValue;
+		for (int i = 0; i < NumValue; ++i) {
+			if (nullptr != _Other.ArrPtr) {
+				ArrPtr[i] = _Other.ArrPtr[i];
+			}
+		}
 	}
+	// 디폴트 소멸자
+	~IntArray()
+	{
+		Release();
+	}
+	// 디폴트 대입연산자
+	void operator=(const IntArray& _Other)
+	{
+		NumValue = _Other.NumValue;
+		for (int i = 0; i < NumValue; ++i) {
+			ArrPtr[i] = _Other.ArrPtr[i];
+		}
+	}
+
+	int& operator[](int _Count)
+	{
+		return ArrPtr[_Count];
+	}
+
+	int& Test(int _Count)
+	{
+		return ArrPtr[_Count];
+	}
+
+	int Num()
+	{
+		return NumValue;
+	}
+
+	void ReSize(int _Size)
+	{
+		if (0 >= _Size)
+		{
+			MsgBoxAssert("배열의 크기가 0일수 없습니다");
+		}
+
+		NumValue = _Size;
+
+		if (nullptr != ArrPtr)
+		{
+			Release();
+		}
+
+		ArrPtr = new int[_Size];
+	}
+
+	void Release()
+	{
+		if (nullptr != ArrPtr)
+		{
+			delete[] ArrPtr;
+			ArrPtr = nullptr;
+		}
+	}
+
+private:
+	int NumValue = 0;
+	int* ArrPtr = nullptr;
 };
 
 
 
-#define Ten 10
 
-#define Multi(Value) Value * Value
 int main()
 {
-	int a = Ten;
-	int b = Multi(a);
+	LeckCheck;
+
+	int arr[3] = {};
+	arr[5] = 30;
 
 
+	{
+		IntArray NewArray0 = IntArray(5);
+		IntArray NewArray1 = IntArray(5);
+
+		NewArray0 = NewArray1;
+
+		NewArray0.~IntArray();
+		NewArray1.~IntArray();
+	}
+
+	{
+		IntArray NewArray0 = IntArray(5);
+		IntArray NewArray1 = IntArray(NewArray0);
+	}
+
+	// 내가 만든 클래스이기 때문에
+	// 배열이 아니었기 때문에
+	// 배열의 기능들을 해결해줘야 한다.
+
+	// 1. 대입이 안된다. => 해결
+	// 2. 크기를 동적으로 바꿀수 없다.
+	// 3. 그 크기를 알수도 없다. 알수는 있는데 불편해
+
+	// NewArray0.~IntArray();
 }
-
-
-//메모리맵을 직접 그려볼건데 조금 복잡한 할당을 만들거임
